fb9833510deff4c5008e39109d8c4fe15a51b65f
roysatadru <satadru.roy98@gmail.com>
2024-09-27 01:16:57 +0530

    ema fix

--- trading_indicators/ema.py
+++ trading_indicators/ema.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def ema(series: pd.Series, period: int):
+  if period < 1:
+    raise ValueError('Period must be greater than 0')
+  if period == 1:
+    return series
   alpha = 2 / (period + 1)
   ema_calc = series.ewm(alpha=alpha, adjust=False, min_periods=period).mean()
   return ema_calc.astype(float)
